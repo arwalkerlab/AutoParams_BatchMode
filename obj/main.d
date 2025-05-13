2d86f94c9e71c52419090d984f1c9870bc0c58c5
Mark Alan Hix <markahix@gmail.com>
2025-05-13 11:36:37 -0400

    Initial Commit

---
+++ obj/main.d
@@ -0,0 +1,22 @@
+obj/main.o: src/main.cpp include/autoparams.h include/utilities.h \
+ include/classes.h include/settings.h include/utilities.h \
+ include/classes.h include/settings.h include/parameters.h \
+ include/optimize.h
+
+include/autoparams.h:
+
+include/utilities.h:
+
+include/classes.h:
+
+include/settings.h:
+
+include/utilities.h:
+
+include/classes.h:
+
+include/settings.h:
+
+include/parameters.h:
+
+include/optimize.h:
